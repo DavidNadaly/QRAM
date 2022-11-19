61afa1d51ccf1c7b6720be56ad0085dd74d80d8c
David Nadaly <vishnupurushothaman56@gmail.com>
2022-11-19 15:55:52 +0530

    explanation of code is corrected

--- Qram.py
+++ Qram.py
@@ -1,4 +1,4 @@
-#importing stamdard Qiskit libraries
+#importing standard Qiskit libraries
 
 import numpy as np
 from qiskit import QuantumCircuit, transpile, Aer, IBMQ, QuantumRegister, ClassicalRegister, execute
@@ -14,9 +14,9 @@ provider = IBMQ.load_account()
 
 quant_state_1 = str("1111001011")
 quant_state_2 = str("1100100101")
+#You can add any 10 bit quantum states
 
-
-qr1 = QuantumRegister(2, name ='a')   #Auxillary register initialized to store 01 state. After uploading first quantum state the auxillary register  will exist in both the state 00 and 01.  The quantum state with 00 state will be stored in memory and the next state to be uploaded will be stored in 01 state  of auxillary register.
+qr1 = QuantumRegister(2, name ='a')   #Auxillary register. It is initialized to store 01 state. After uploading first quantum state the auxillary register  will exist in  the state 00 and 01.  The  state with 00  will be entangled with the quantum state stored in memory and the next state to be uploaded will be entangled with 01 state  of auxillary register.
 qr2 = QuantumRegister(10, name = 'm') #This is the memory  register where quant states are stored.
 qc = QuantumCircuit(qr1,qr2,)
 qc.x(qr1[1]) #Auxillary register initialized to 01 state
@@ -28,7 +28,7 @@ def memory_circuit(qc,quant_state):
     for i in range(len(quant_state)-1,-1,-1):
         if (int(quant_state[i]) == 0): 
             qc.x(qr2[m]) #X gate is applied if the state to be uploaded is 0
-            m = m+1 #During each iteration the gate is applied from 0th qubit, to 1st, 2nd, 3rd and so on.
+            m = m+1 #During each iteration the gate is applied from 0th qubit to 1st qubit, 2nd, 3rd and so on.
         if (int(quant_state[i]) == 1):
             qc.cx(qr1[1],qr2[m]) #CX gate is applied on  memory register and auxillary register if the state to be  uploaded is 1. 
             m = m+1
@@ -66,11 +66,11 @@ qc.barrier()
 
 #uploading 2nd quantum  state
 memory_circuit(qc,quant_state_2) #applying memory_circuit fn on memory register
-qc.mct([qr2[0],qr2[1],qr2[2],qr2[3],qr2[4],qr2[5],qr2[6],qr2[7],qr2[8],qr2[9]],qr1[0]) #applying multicontrol gate, targeted on 0th qubit of auxillary registed controlled by 10 qubits of memory register
+qc.mct([qr2[0],qr2[1],qr2[2],qr2[3],qr2[4],qr2[5],qr2[6],qr2[7],qr2[8],qr2[9]],qr1[0]) #applying multicontrol gate, targeted on 0th qubit of auxillary register controlled by 10 qubits of memory register
 qc.append(unitary_2(), [qr1[0]]+[qr1[1]]) #appending unitary gate
 qc.mct([qr2[0],qr2[1],qr2[2],qr2[3],qr2[4],qr2[5],qr2[6],qr2[7],qr2[8],qr2[9]],qr1[0]) #applying the same multicontrol gate again
 memory_circuit(qc,quant_state_2) #calling memory_circuit function again
 qc.barrier()
 qc.draw()
 
-#The state to be uploaded has been now uploaded and being stored in memory register. By measuring the memory register, we can check whether the state was properly uploaded
+#The state to be uploaded have now been uploaded and stored in memory register. By measuring the memory register, we can check whether the state was properly uploaded. To add quantum states of specific amplitude, unitary gates have to be designed accordingly
